8236578cda89706d866451b951c2b679c286b9c3
yogesan66 <yogesan10@gmail.com>
2025-07-23 20:46:01 +0530

    fixed ui issue

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,23 @@ function App() {
   return (
     <div className=" relative w-screen min-h-screen overflow-y-auto bg-theme-light-bg dark:bg-theme-dark-bg font-inter">
       <div className="bg-theme-light-topBg dark:bg-theme-dark-topBg h-64 sm:h-60"></div>
-      <div className="absolute p-7 md:p-0 top-0 flex flex-col  justify-self-center max-w-max ">
-        <Navbar />
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-7">
-          {socialMediaList.map((item, index) => (
-            <SocialMediaCard key={index} data={item} />
-          ))}
-        </div>
-        <div className="mt-12 pb-7">
-          <p className="text-theme-light-textMuted  dark:text-theme-dark-textWhite font-bold text-2xl mb-6">
-            Overview - Today
-          </p>
+      <div className="absolute top-0 left-0 w-full p-7 md:p-0 flex justify-center">
+        <div>
+          <Navbar />
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-7">
-            {overviewList.map((item, index) => (
-              <OverviewCard key={index} data={item} />
+            {socialMediaList.map((item, index) => (
+              <SocialMediaCard key={index} data={item} />
             ))}
+          </div>
+          <div className="mt-12 pb-7">
+            <p className="text-theme-light-textMuted dark:text-theme-dark-textWhite font-bold text-2xl mb-6">
+              Overview - Today
+            </p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-7">
+              {overviewList.map((item, index) => (
+                <OverviewCard key={index} data={item} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
